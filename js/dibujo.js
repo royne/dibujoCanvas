03fb1948ec4ca6b9d047d2b3774d1a1208fc7293
Royne Chaves <royne.chaves@gmail.com>
2017-11-18 23:15:10 -0500

    dibuja lineas con clicks de un punto inicial a un final

--- js/dibujo.js
+++ js/dibujo.js
@@ -2,25 +2,36 @@ canvas.addEventListener("mouseup", hacerClick);
 canvas.addEventListener("mousedown", soltarClick);
 var cuadro = document.getElementById("canvas");
 var area_dibujo = cuadro.getContext("2d");
+var color = document.getElementById("eligeColor");
+var colorLinea;
+var xi;
+var yi;
+var xf;
+var yf;
 
 function hacerClick(evento)
 {
-    console.log(evento);
+    xi = evento.layerX;
+    yi = evento.layerY;
 }
 
 function soltarClick(evento2)
 {
-    console.log(evento2);
+    xf = evento2.layerX;
+    yf = evento2.layerY;
+    colorLinea = color.value
+    dibujarLinea(colorLinea, xi, yi, xf, yf, area_dibujo);
 }
 
 //dibujarLinea("red", 120, 110, 151, 151, area_dibujo);
 
-function dibujarLinea(color, xi, yi, xf, yf, lienzo)
+function dibujarLinea(colorLinea, xi, yi, xf, yf, lienzo)
 {
     lienzo.beginPath();
-    lienzo.strokeStyle = color;
+    lienzo.strokeStyle = colorLinea;
     lienzo.moveTo(xi, yi);
     lienzo.lineTo(xf, yf);
     lienzo.stroke();
     lienzo.closePath();
 }
+console.log(color.value);
