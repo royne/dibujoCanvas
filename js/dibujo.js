9f010d3a9ed6cdc687c9d08e5d0879dc0d9b7cef
Royne Chaves <royne.chaves@gmail.com>
2017-11-18 23:50:15 -0500

    se dibuja con movimineto del mouse y boton borrar

--- js/dibujo.js
+++ js/dibujo.js
@@ -1,5 +1,7 @@
 canvas.addEventListener("mouseup", hacerClick);
 canvas.addEventListener("mousedown", soltarClick);
+canvas.addEventListener("mousemove", dibujar);
+var clear = document.getElementById("Borrar");
 var cuadro = document.getElementById("canvas");
 var area_dibujo = cuadro.getContext("2d");
 var color = document.getElementById("eligeColor");
@@ -8,22 +10,32 @@ var xi;
 var yi;
 var xf;
 var yf;
+var iniciar = false
 
-function hacerClick(evento)
+function soltarClick(evento)
 {
+    iniciar = true;
     xi = evento.layerX;
     yi = evento.layerY;
 }
 
-function soltarClick(evento2)
+function dibujar(evento2)
 {
+  if (iniciar) {
     xf = evento2.layerX;
     yf = evento2.layerY;
     colorLinea = color.value
     dibujarLinea(colorLinea, xi, yi, xf, yf, area_dibujo);
+		xi = xf;
+		yi = yf;
+  }
+
 }
 
-//dibujarLinea("red", 120, 110, 151, 151, area_dibujo);
+function hacerClick(){
+    iniciar= false
+}
+
 
 function dibujarLinea(colorLinea, xi, yi, xf, yf, lienzo)
 {
@@ -34,4 +46,8 @@ function dibujarLinea(colorLinea, xi, yi, xf, yf, lienzo)
     lienzo.stroke();
     lienzo.closePath();
 }
-console.log(color.value);
+
+
+clear.addEventListener("click",function(){
+	canvas.width=canvas.width;
+},false);
